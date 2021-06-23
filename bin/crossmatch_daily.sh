fb3cd628158f03297e464f5b3d4b9ef05e3cc4fe
David Wang <ykwang@uw.edu>
2021-06-23 11:33:41 -0700

    change cron to use production version

--- bin/crossmatch_daily.sh
+++ bin/crossmatch_daily.sh
@@ -4,12 +4,12 @@ today=$(date +"%y%m%d")
 
 source  "/epyc/opt/anaconda-2019/etc/profile.d/conda.sh"
 conda activate ztf_kafka
-cd /epyc/users/ykwang/Github/alert_stream_crossmatch/bin
+cd /epyc/users/ykwang/Github/production/alert_stream_crossmatch/bin
 
 ./run_crossmatch_simple $today 2 _pid2
 ./run_crossmatch_simple $today 1 _pid2
 
 
 mkdir -p ~/data/ztf_db_backups/$today/
-cp ../local/db/sqlite_pid2.db ~/data/ztf_db_backups/$today/sqlite_pid2.db
+cp ../local/db/sqlite_pid2.db ~/data/backups/$today/sqlite_pid2.db
 # running one pid after the other on purpose (get denser historical data from pid2 first)
--- bin/crossmatch_daily_pid1.sh
+++ bin/crossmatch_daily_pid1.sh
@@ -4,9 +4,9 @@ today=$(date +"%y%m%d")
 
 source  "/epyc/opt/anaconda-2019/etc/profile.d/conda.sh"
 conda activate ztf_kafka
-cd /epyc/users/ykwang/Github/alert_stream_crossmatch/bin
+cd /epyc/users/ykwang/Github/production/alert_stream_crossmatch/bin
 ./run_crossmatch_simple $today 1 simple 
 
 mkdir -p ~/data/ztf_db_backups/$today/
-cp ../local/db/sqlitesimple.db ~/data/ztf_db_backups/$today/sqlitesimple.db
+cp ../local/db/sqlitesimple.db ~/data/backups/$today/sqlitesimple.db
 # running one pid after the other on purpose (get denser historical data from pid2 first)
