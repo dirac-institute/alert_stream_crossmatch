91c6810ffb365d2bb810998881d0ccce6af54121
David Wang <ykwang@uw.edu>
2021-07-16 16:39:04 -0700

    ignore SS objects

--- alert_stream_crossmatch/simple_crossmatch.py
+++ alert_stream_crossmatch/simple_crossmatch.py
@@ -308,6 +308,10 @@ def process_packet(packet, xray_skycoord, dfx, saved_packets, sources_seen, data
     """Examine packet for matches in the ROSAT database. Save object to database if match found"""
     if packet["candidate"]["drb"] < 0.8:  # if packet real/bogus score is low, ignore
         return
+    # # Not a solar system object (or no known obj within 5")
+    if (packet["candidate"]['ssdistnr'] is None) or (packet["candidate"]['ssdistnr'] < 0) or (packet["candidate"]['ssdistnr'] > 5):
+        return
+
     ztf_source = get_candidate_info(packet)
     conn = create_connection(database)
     if packet["objectId"] in sources_seen:
