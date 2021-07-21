199c39c55d24a458b455caf5b9abc99a54b486b7
David Wang <ykwang@uw.edu>
2021-07-20 17:23:10 -0700

    move directories into a config file

--- bin/crossmatch_daily.sh
+++ bin/crossmatch_daily.sh
@@ -1,14 +1,15 @@
 #! /bin/bash
 
+. ./setup.cfg
 today=$(date +"%y%m%d")
 
 source  "/epyc/opt/anaconda-2019/etc/profile.d/conda.sh"
 conda activate ztf_kafka
-cd /epyc/users/ykwang/Github/alert_stream_crossmatch/bin
+cd $working_dir/bin
 
-./run_crossmatch_simple $today 2 _pid2
-./run_crossmatch_simple $today 1 _pid2
+./run_crossmatch_simple $today 2 $db_suffix
+./run_crossmatch_simple $today 1 $db_suffix
 
-mkdir -p ~/data/backups/$today/
-cp ../local/db/sqlite_pid2.db ~/data/backups/$today/sqlite_pid2.db
+mkdir -p $backup_dir/$today/
+cp ../local/db/sqlite_pid2.db $backup_dir/$today/sqlite_pid2.db
 # running one pid after the other on purpose (get denser historical data from pid2 first)
